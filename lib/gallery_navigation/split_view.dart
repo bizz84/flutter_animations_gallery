5d7c840c141c43cfd9f23323c962ad809720aa2a
bizz84 <bizz84@gmail.com>
2021-07-07 22:20:20 +0100

    Make SplitView properties configurable

--- lib/gallery_navigation/split_view.dart
+++ lib/gallery_navigation/split_view.dart
@@ -2,22 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:flutter_animations_gallery/gallery_navigation/split_view_show_menu_button.dart';
 
 class SplitView extends StatefulWidget {
-  const SplitView(
-      {Key? key, required this.menuBuilder, required this.contentBuilder})
-      : super(key: key);
+  const SplitView({
+    Key? key,
+    required this.menuBuilder,
+    required this.contentBuilder,
+    this.breakpoint = 600,
+    this.menuWidth = 240,
+    this.menuRevealDuration = const Duration(milliseconds: 250),
+    this.menuRevealCurve = Curves.easeInOutCubic,
+  }) : super(key: key);
   final WidgetBuilder menuBuilder;
   final WidgetBuilder contentBuilder;
+  final double breakpoint;
+  final double menuWidth;
+  final Duration menuRevealDuration;
+  final Curve menuRevealCurve;
 
   @override
   SplitViewState createState() => SplitViewState();
 }
 
 class SplitViewState extends State<SplitView> {
-  static const duration = Duration(milliseconds: 250);
-  static const curve = Curves.easeInOutCubic;
-  static const menuContainerWidth = 240.0;
-  static const splitViewBreakpoint = 600.0;
-
   bool _showMenu = false;
   void toggleMenu() {
     setState(() => _showMenu = !_showMenu);
@@ -26,42 +31,43 @@ class SplitViewState extends State<SplitView> {
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
-    if (screenWidth >= splitViewBreakpoint) {
-      // wide screen: return split-view menu on the left, content on the right
+    if (screenWidth >= widget.breakpoint) {
+      // wide screen: menu on the left, content on the right
       return Row(
         children: [
           SizedBox(
-            width: menuContainerWidth,
+            width: widget.menuWidth,
             child: widget.menuBuilder(context),
           ),
           Expanded(child: widget.contentBuilder(context)),
         ],
       );
     } else {
-      // narrow screen: menu slides in/out above the content view
+      // narrow screen: show content, menu slides in above it
       return Stack(
         children: [
+          // TODO: gesture detector to dismiss menu when content area is tapped
           Positioned.fill(
             child: AnimatedOpacity(
-              duration: duration,
-              curve: curve,
+              duration: widget.menuRevealDuration,
+              curve: widget.menuRevealCurve,
               opacity: _showMenu ? 0.5 : 1.0,
               child: widget.contentBuilder(context),
             ),
           ),
           AnimatedPositioned(
-            duration: duration,
-            curve: curve,
-            left: _showMenu ? 0 : -menuContainerWidth,
-            width: menuContainerWidth,
+            duration: widget.menuRevealDuration,
+            curve: widget.menuRevealCurve,
+            left: _showMenu ? 0 : -widget.menuWidth,
+            width: widget.menuWidth,
             top: 0,
             bottom: 0,
             child: widget.menuBuilder(context),
           ),
           AnimatedPositioned(
-            duration: duration,
-            curve: curve,
-            left: _showMenu ? menuContainerWidth : 0,
+            duration: widget.menuRevealDuration,
+            curve: widget.menuRevealCurve,
+            left: _showMenu ? widget.menuWidth : 0,
             bottom: 0,
             child: SafeArea(
               child: SplitViewShowMenuButton(
